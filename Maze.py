7669665e253864dda94eeb396117d73e43e70742
fcwe1113 <fcwe1113@gmail.com>
2025-10-29 13:45:47 +0000

    implemented dist matrix and heuristic method

--- Maze.py
+++ Maze.py
@@ -6,6 +6,7 @@ import Node
 class Maze:
 
     Maze: list[list[Node.Node]] = []
+    dist: list[list[int]] = [] # will be initialized to -1
     startx: int
     starty: int
     endx: int
@@ -23,9 +24,12 @@ class Maze:
 
         for i in range(len(maze)):
             self.Maze.append([])
+            self.dist.append([])
             for j in range(len(maze[i])):
                 self.Maze[i].append(Node.Node(i, j, maze[i][j][0], maze[i][j][1], maze[i][j][2], maze[i][j][3]))
+                self.dist[i].append(-1)
 
+        self.dist_init()
         # raise a warning if start and/or end node is placed in isolated coord
         # no i am not integrating a search algorithm just to check can the 2 reach each other thats the point of the project lmao
         if self.Maze[starty][startx].is_isolated():
@@ -60,6 +64,39 @@ class Maze:
 
     def is_end(self, x, y):
         return x == self.endx and y == self.endy
+
+    def dist_init(self):
+        self.dist[self.endy][self.endx] = 0
+
+        def set_dist(dist, x, y): # up
+            if y != 0:
+                if self.dist[y - 1][x] == -1: # check is traversed already
+                    if self.traversable(x, y - 1, x, y): # check if above block can travel down
+                        self.dist[y - 1][x] = dist + 1
+                        set_dist(dist + 1, x, y - 1) # recurse into the bloc above
+
+            if y != len(self.Maze) - 1: # down
+                if self.dist[y + 1][x] == -1:
+                    if self.traversable(x, y + 1, x, y):
+                        self.dist[y + 1][x] = dist + 1
+                        set_dist(dist + 1, x, y + 1)
+
+            if x != 0: # left
+                if self.dist[y][x - 1] == -1:
+                    if self.traversable(x - 1, y, x, y):
+                        self.dist[y][x - 1] = dist + 1
+                        set_dist(dist + 1, x - 1, y)
+
+            if x != len(self.Maze[y]) - 1: # down
+                if self.dist[y][x + 1] == -1:
+                    if self.traversable(x + 1, y, x, y):
+                        self.dist[y][x + 1] = dist + 1
+                        set_dist(dist + 1, x + 1, y)
+
+        set_dist(0, self.endx, self.endy)
+
+    def heuristic(self, x1, y1, x2, y2):
+        return abs(x1 - x2) + abs(y1 - y2)
 
     def __str__(self):
         # the tostring function of the maze
@@ -114,3 +151,10 @@ class Maze:
                     output += "----"
             output += "-\n"
         return output + "Start: s\tEnd: e\tOne way paths: arrows\n"
+
+    def print_dist(self):
+        output = str(self.dist[0]) + "\n"
+        for row in range(1, len(self.dist)):
+            output += str(self.dist[row]) + "\n"
+
+        return output
--- main.py
+++ main.py
@@ -10,14 +10,14 @@ MAZE = [[[False, False, False, True], [False, True, False, True], [False, False,
         [[True, True, False, True], [False, False, True, True], [True, False, True, False], [True, True, False, False], [True, True, False, True], [False, False, True, True], [False, True, True, True], [False, True, True, True], [False, True, True, False], [True, True, False, False]],
         [[True, False, False, True], [False, True, True, False], [False, True, False, True], [True, False, True, False], [True, True, False, True], [False, False, True, False], [True, False, False, True], [True, True, True, False], [True, True, False, False], [True, True, False, False]],
         [[False, True, False, False], [True, True, False, False], [True, True, False, False], [False, False, False, True], [True, False, True, False], [False, True, False, True], [False, True, True, False], [True, True, False, False], [True, True, False, False], [True, True, False, False]],
-        [[True, False, False, True], [True, False, True, False], [True, True, False, False], [False, False, False, True], [False, False, True, True], [True, False, True, False], [True, True, False, False], [True, True, False, False], [True, False, False, True], [True, False, True, False]],
-        [[False, True, False, True], [False, True, True, False], [True, False, False, True], [False, False, True, False], [False, True, False, True], [False, True, True, False], [True, True, False, False], [True, True, False, False], [False, True, False, True], [False, False, True, False]],
+        [[True, True, False, True], [True, False, True, False], [True, True, False, False], [False, False, False, True], [False, False, True, True], [True, False, True, False], [True, True, False, False], [True, True, False, False], [True, False, False, True], [True, False, True, False]],
+        [[True, True, False, True], [False, True, True, False], [True, False, False, True], [False, False, True, False], [False, True, False, True], [False, True, True, False], [True, True, False, False], [True, True, False, False], [False, True, False, True], [False, False, True, False]],
         [[True, True, False, False], [True, False, False, True], [False, True, True, True], [False, False, True, True], [True, False, True, False], [True, False, False, True], [True, False, True, False], [True, False, False, True], [True, False, True, True], [False, True, True, False]],
         [[True, False, False, True], [False, True, True, False], [True, True, False, False], [False, True, False, True], [False, False, True, True], [False, False, True, True], [False, False, True, True], [False, True, True, False], [False, True, False, False], [True, True, False, False]],
         [[False, False, False, True], [True, False, True, False], [True, False, False, True], [True, False, True, True], [False, False, True, False], [False, False, False, True], [False, False, True, True], [True, False, True, False], [True, False, False, True], [True, False, True, False]]]
 
 
 if __name__ == '__main__':
-    environment = Maze.Maze(MAZE, 0, 0, 7, 1)
+    environment = Maze.Maze(MAZE, 0, 0, 9, 9)
     print(environment)
-
+    print(environment.print_dist())
